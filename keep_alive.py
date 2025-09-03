e002394d5b5e1bd466bbfbf27b2ba80fceb97c28
mirayfrrr <wsrj17s@gmail.com>
2025-09-03 21:38:10 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from threading import Thread
 
@@ -8,7 +9,9 @@ def home():
     return "✅ Bot çalışıyor!"
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))  # Render hangi portu verdiyse onu al
+    app.run(host="0.0.0.0", port=port)
 
 def keep_alive():
-    t = Thread(target
+    t = Thread(target=run)
+    t.start()
